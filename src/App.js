b59e0630c2c4dc7d66c90e9c1238d708ada5b369
Krishna <krishnakeec@gmail.com>
2022-10-22 21:35:26 +0300

    input name handled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+const handleNameChange = (event) => {
+  event.preventDefault()
+  setNewName(event.target.value)  
+} 
 
 
-function App() {
-  return (
-    <div className="App">
-      hellp
-    </div>
-  );
+const addName = (e) => {
+  e.preventDefault()
+  const person = {name: newName}
+  setPersons([...persons,person])
 }
 
-export default App;
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(
+        (person) => {
+          return(
+            <div key={person.name}>{person.name}</div>
+          )
+        }
+        )
+      }
+    </div>
+  )
+}
+
+export default App
